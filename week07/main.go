02bef4659170239a76f65e5ce0ecb2f70b1958e0
dhman <doheonman@naver.com>
2024-10-11 17:23:43 +0900

    v1.0 If

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -10,6 +11,11 @@ func main() {
 	fmt.Print("이름 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	name, err := in.ReadString('\n') // err을 하나 더 주었기때문에 ReadString이 오류 나지 않음
-	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
+
 }
