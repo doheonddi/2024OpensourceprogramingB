23aaddc9035270dd38ab114ebc1a437be0be9cbb
dhman <doheonman@naver.com>
2024-10-11 17:53:31 +0900

    v1.1 Shadow

--- week07/main.go
+++ week07/main.go
@@ -1,21 +1,12 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	fmt.Print("이름 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	name, err := in.ReadString('\n') // err을 하나 더 주었기때문에 ReadString이 오류 나지 않음
+	// var float32 float32 = 9.1
+	// fmt.Println(float32)
+	// var number float
+	// fmt.Println(number)
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(name)
-	}
+	var fmt float32 = 9.1
+	fmt.Println(float32)
 
 }
