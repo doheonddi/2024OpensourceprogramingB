3fea1ca5b738610d23af69c33ec27ddcf763b0fd
dhman <doheonman@naver.com>
2024-10-25 17:19:23 +0900

    v1.9 Guess game

--- week09/main.go
+++ week09/main.go
@@ -14,8 +14,9 @@ import (
 func main() {
 	rand.Seed(time.Now().Unix()) // 초 단위로 바꿈
 	answer := rand.Intn(6) + 1
-	fmt.Println(answer)
 
+	var win bool
+	win = true
 	for guesses := 0; guesses < 3; guesses++ {
 		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
 		in := bufio.NewReader(os.Stdin)
@@ -41,5 +42,11 @@ func main() {
 			fmt.Println("입력하신 값은 정답보다 큰 수 입니다. HIGH")
 		}
 
+	}
+
+	if win {
+		fmt.Println("당신이 이겼습니다!")
+	} else {
+		fmt.Printf("당신이 졌습니다. 정답은 %d입니다.\n", answer)
 	}
 }
