feb31860dc3154b300e9fd91ec421dced11242a2
dhman <doheonman@naver.com>
2024-11-01 17:59:10 +0900

    v2.7 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	fmt.Printf("%f\n", math.Sqrt(25.0))
 	fmt.Print("정수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -30,12 +32,13 @@ func main() {
 		isPrime = false
 	} else {
 		i := 2
-		for i < n {
+		//for i < n {
+		for i <= int(math.Sqrt(float64(n))) {
 			if n%i == 0 {
 				isPrime = false
-				break
+				break // 1과 자기자신을 제외한 첫 번째 약수가 발견 되면 반복문 종료
 			}
-			fmt.Printf("%d", i) // 반복 횟수 확인용 코드
+			fmt.Printf("%d ", i) // 반복 횟수 확인용 코드
 			i++
 		}
 	}
