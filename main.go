3843225da8dfce7e9e717be99f3063a8a100326f
dhman <doheonman@naver.com>
2024-11-22 17:00:14 +0900

    v4.3 Slice

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	var emptySlice []bool
+	// emptySlice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptySlice) // slice zero value (nii)
+
 	gpas := [5]float64{3.5, 4.1, 4.5, 4.9, 4.23} // array := array literal
 	gpa_slice := gpas[1:4]                       // {4.1, 4.5, 4.9}
 	// gpa_slice[1] = 2.71
